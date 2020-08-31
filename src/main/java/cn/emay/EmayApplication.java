ad1366dde12e247e5d11f274ea4ec44a4887feb1
frank <frankup@yeah.net>
2020-08-31 23:52:52 +0800

    1. 升级super-scheduler，修复bug；2. 把 OnlyBusinessCode 和 初始化业务逻辑的加载时间调整到spring容器初始化完成之前；确保spring对外提供服务时所有的资源都加载完毕了。

--- src/main/java/cn/emay/EmayApplication.java
+++ src/main/java/cn/emay/EmayApplication.java
@@ -1,24 +1,14 @@
 package cn.emay;
 
-import cn.emay.redis.RedisClient;
-import cn.emay.utils.ApplicationContextUtils;
-import cn.emay.utils.OnlyBusinessCode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.annotation.Resource;
 
 /**
  * 启动入口类
@@ -36,41 +26,10 @@ public class EmayApplication {
         SpringApplication.run(EmayApplication.class, args);
     }
 
-    /**
-     * 定时续租业务编码
-     */
-    @Scheduled(initialDelay = 10000, fixedDelay = 10000)
-    protected void heartBeatTask() {
-        RedisClient redis = ApplicationContextUtils.getBean(RedisClient.class);
-        OnlyBusinessCode.renew(redis);
-    }
-
-    @EventListener
-    public void closed(ContextClosedEvent event) {
-        RedisClient redis = ApplicationContextUtils.getBean(RedisClient.class);
-        OnlyBusinessCode.unsubscribe(redis);
-    }
-
-    @Component
-    public static class CheckParamsApplicationRunner implements ApplicationRunner {
-
-        Logger log = LoggerFactory.getLogger(getClass());
-
-        @Resource
-        private RedisClient redis;
-
-        @Override
-        public void run(ApplicationArguments args) {
-            try {
-                OnlyBusinessCode.occpy(redis);
-                log.info("BUSINESS_CODE:[{}]", OnlyBusinessCode.getCode());
-            } catch (Exception e) {
-                log.error("error", e);
-                SpringApplication.exit(ApplicationContextUtils.getApplicationContext(), () -> -999);
-            }
-
-        }
-
+    @Order(2)
+    @EventListener(ContextRefreshedEvent.class)
+    public void start() {
+        // 这里可以在启动前加载其他资源
     }
 
 }
